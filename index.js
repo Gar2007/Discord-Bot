35069486512d93c5b8edfb5a470bc0734d72e9d5
Gar2007 <66054120+Gar2007@users.noreply.github.com>
2021-03-05 13:20:57 +0700

    Dark Skeleton Team

---
+++ index.js
@@ -0,0 +1,16 @@
+const api = require('discord.js');
+const bot = new api.Client();
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!\n`);
+  console.log("XZone Discord Bot");
+  console.log("Created By Gar2007@Dark Skeleton Team");
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+bot.login('NzgzOTI1MzYyNDQxNTg0Njgx.X8h1Yw.-WUTEBz3ZFiX9ngBcaRqF-TtLoI');
